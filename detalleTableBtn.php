0f4d1b015fb9c8b34f81f656d0a34ff6d6436b4f
Steven <stevgomezt@gmail.com>
2022-10-18 09:11:57 -0500

    avances

---
+++ detalleTableBtn.php
@@ -0,0 +1,77 @@
+<?php
+if (isset($_POST['search'])) {
+    $valueToSearh = $_POST['valueToSearh'];
+    $query = "SELECT * FROM enlaces_detalle_concursos WHERE 
+       cartel LIKE '%" . $valueToSearh . "%'";
+    $result = filterRecord($query);
+} else {
+    $query = "SELECT * FROM enlaces_detalle_concursos";
+    $result = filterRecord($query);
+}
+
+function filterRecord($query)
+{
+    include "conect.php";
+    $filter_result = mysqli_query($mysqli, $query);
+    return $filter_result;
+}
+
+?>
+<!DOCTYPE html>
+<html>
+
+<head>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <link rel="stylesheet" href="styles.css">
+</head>
+
+<body class="body">
+    <header id="header">
+        <div class="header" style="display: flex !important;flex-direction: row !important;">
+            <div class="row">
+                <div class="col-2 sidebar">
+                    <img src="src/scraping.png" alt="">
+                    <h4 class="volver">Scraping www.sicop.go.cr</h4>
+                    <ul class="nav nav-pills nav-sidebar">
+                    </ul>
+                </div>
+                <div>
+                    <input class="busqueda" id="busquedaInput" type="text" placeholder="Filtre aqui por palabra clave" />
+                </div>
+                <div class="busqueda col-2">
+                    <form class="" action=" <?php echo $_SERVER["PHP_SELF"]; ?>" method="post">
+                    </form>
+                </div>
+            </div>
+        </div>
+    </header>
+    <?php
+    echo "<table class='table table-striped '>
+    <thead class='thead-dark'>
+    
+    <tr>
+    <th>#</th>
+    <th># Procedimiento</th>
+    <th>Boton</th>
+    <th></th>
+    </tr>
+    </thead>";
+    while ($row = mysqli_fetch_array($result)) {
+        echo "<tbody id='busquedaTable'>";
+        echo "<tr>";
+        echo "<td>" . $row['id'] . "</td>";
+        echo "<td>" . $row['concurso_id'] . "</td>";
+        echo "<td>" . $row['nombre'] . "</td>";
+        echo "<td> <a target='_blank' href='" . $row['link'] . "' > Revisar </a> </td>";
+        echo "</tr>";
+        echo "<tbody";
+    }
+    echo "</table>";
+    ?>
+    </div>
+    <script src="javascript.js"></script>
+    <script src="filtrar.js"></script>
+</body>
+
+</html>
---
+++ filtrar.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+    $("#busquedaInput").on("keyup", function () {
+        var value = $(this).val().toLowerCase();
+        $("#busquedaTable tr").filter(function () {
+            $(this).toggle(
+                $(this).text().toLowerCase().indexOf(value) > -1
+            );
+        });
+    });
+});
